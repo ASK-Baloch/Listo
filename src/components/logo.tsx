80ae4a3a2d34640327d20fc1404a633c649a0b6d
Ahmed <balochsaeed888@gmail.com>
2024-03-31 16:58:11 +0500

    imported font and used in page.tsx and setting up the logo

---
+++ src/components/logo.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import { cn } from "@/lib/utils";
+import localFont from "next/font/local";
+
+
+const headingFont = localFont({
+    src: "../../../public/fonts/font.woff2",
+})
+export const Logo = () => {
+  return (
+    <Link href="/">
+      <div className={cn("hover:opacity-75 transition items-center gap-x-2 hidden md:flex", headingFont.className)}>
+        <Image src="/logo.svg"
+         alt="Listo logo"
+          width={30}
+           height={30} />
+           <p className="text-lg text-neutral-700 pb-1">Listo</p>
+      </div>
+    </Link>
+  );
+};
