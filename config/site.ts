87ab7ff113f0ea7e5cf365e44f2cdece2de3b7a5
Ahmed <balochsaeed888@gmail.com>
2024-04-03 14:53:18 +0500

    changed the metadata of website , title and desc and added icon too

---
+++ config/site.ts
@@ -0,0 +1,6 @@
+export const siteConfig = {
+    name: "Listo",
+    description: "Collaborate, manage projects, and reach new productivity peaks",
+    // url: "https://listo.vercel.app",
+    // ogImage: "https://listo.vercel.app/og.png",
+}
