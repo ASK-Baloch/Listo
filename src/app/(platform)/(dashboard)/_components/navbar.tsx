3c29c86d42567cec0a98d632e5c95fada75dc915
Ahmed <balochsaeed888@gmail.com>
2024-04-07 19:29:05 +0500

    added logo

--- src/app/(platform)/(dashboard)/_components/navbar.tsx
+++ src/app/(platform)/(dashboard)/_components/navbar.tsx
@@ -1,7 +1,15 @@
+import { Logo } from "@/components/logo";
+
 export const Navbar =()=>{
     return(
-        <div className="fixed z-50">
-            Navbar
+        <div className="fixed z-50 top-0 w-full h-14 border-b shadow-sm bg-white flex items-center">
+            {/* TODO: MOBILE SIDEBAR */}
+            <div className="flex items-centerg gap-x-4 ">
+                <div className="hidden md:flex">
+                    <Logo />
+                </div>
+            </div>
+            
         </div>
     )
 }
