592f7d564ab2669b6ceeed2c95aa1e6f7714093e
Ahmed <balochsaeed888@gmail.com>
2024-04-07 19:32:06 +0500

    changed tags

--- src/app/(platform)/(dashboard)/_components/navbar.tsx
+++ src/app/(platform)/(dashboard)/_components/navbar.tsx
@@ -2,7 +2,7 @@ import { Logo } from "@/components/logo";
 
 export const Navbar =()=>{
     return(
-        <div className="fixed z-50 top-0 w-full h-14 border-b shadow-sm bg-white flex items-center">
+        <nav className="fixed z-50 top-0 px-4 w-full h-14 border-b shadow-sm bg-white flex items-center">
             {/* TODO: MOBILE SIDEBAR */}
             <div className="flex items-centerg gap-x-4 ">
                 <div className="hidden md:flex">
@@ -10,7 +10,7 @@ export const Navbar =()=>{
                 </div>
             </div>
             
-        </div>
+        </nav>
     )
 }
 
