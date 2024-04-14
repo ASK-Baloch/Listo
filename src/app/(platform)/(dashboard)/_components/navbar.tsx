f66be9897ac18f546a559bff4293917d28949be8
Ahmed <balochsaeed888@gmail.com>
2024-04-14 23:27:49 +0500

    upgraded button shadi in navbar now working on sidebar

--- src/app/(platform)/(dashboard)/_components/navbar.tsx
+++ src/app/(platform)/(dashboard)/_components/navbar.tsx
@@ -12,41 +12,46 @@ export const Navbar = () => {
           <Logo />
         </div>
         <Button
+          variant="primary"
           size="sm"
           className="rounded-sm hidden md:block h-auto py-1.5 mb-2 px-2"
         >
           Create
         </Button>
-        <Button size='sm' className="rounded-sm block md:hidden">
-            <Plus className="w-4 h-4" />
+        <Button
+          variant="primary"
+          size="sm"
+          className="rounded-sm block md:hidden"
+        >
+          <Plus className="w-4 h-4" />
         </Button>
       </div>
       <div className="ml-auto flex items-center gap-x-2">
-        <OrganizationSwitcher 
-        hidePersonal
-        afterCreateOrganizationUrl="/organization/:id"
-        afterLeaveOrganizationUrl="/select-org"
-        afterSelectOrganizationUrl='/organization/:id'
-        appearance={{
-            elements:{
-                rootBox:{
-                   display:"flex",
-                   justifyContent:"center",
-                   alignItems:"center",
-                }
-            }
-        }}
+        <OrganizationSwitcher
+          hidePersonal
+          afterCreateOrganizationUrl="/organization/:id"
+          afterLeaveOrganizationUrl="/select-org"
+          afterSelectOrganizationUrl="/organization/:id"
+          appearance={{
+            elements: {
+              rootBox: {
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              },
+            },
+          }}
         />
-        <UserButton 
-        afterSignOutUrl="/"
-        appearance={{
-            elements:{
-                avatarBox:{
-                    height:30,
-                    width:30,
-                }
-            }
-        }}
+        <UserButton
+          afterSignOutUrl="/"
+          appearance={{
+            elements: {
+              avatarBox: {
+                height: 30,
+                width: 30,
+              },
+            },
+          }}
         />
       </div>
     </nav>
