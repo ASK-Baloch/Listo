2cf331e4d146bb04e95792b7da1ad7e0a245336f
Ahmed <balochsaeed888@gmail.com>
2024-04-14 23:11:08 +0500

    working in src/middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,22 @@
 import { authMiddleware } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
  
 export default authMiddleware({
-    publicRoutes:['/']
+    publicRoutes:['/'],
+    afterAuth(auth,req){
+      if(auth.userId && auth.isPublicRoute){
+        let path = "/select-org";
+
+        if(auth.orgId){
+          path = `/organization/${auth.orgId}`;
+        }
+
+        const orgSelection = new URL(path, req.url);
+        return NextResponse.redirect(orgSelection);
+      }
+      if(!auth.userId && !auth.isPublicRoute){
+        return
+    }
 });
  
 export const config = {
