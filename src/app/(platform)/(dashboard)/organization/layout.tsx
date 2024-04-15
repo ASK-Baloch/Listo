e602d7817a6eb0e3d8ea6342d464a97f4bd390fa
Ahmed <balochsaeed888@gmail.com>
2024-04-15 23:40:21 +0500

    installed dependencies and started component Sidebar

--- src/app/(platform)/(dashboard)/organization/layout.tsx
+++ src/app/(platform)/(dashboard)/organization/layout.tsx
@@ -4,7 +4,7 @@ const OrganizationLayout = ({ children }: { children: React.ReactNode }) => {
       <div className="flex gap-x-7">
         <div className="w-64
          shrink-0 hidden md:block">
-            {/* TODO: SIDEBAR */}
+            <Sidebar />
          </div>
         {children}
       </div>
