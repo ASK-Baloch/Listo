9854a7a111800f9d66ef47848ff7a3a23f19e367
Ahmed <balochsaeed888@gmail.com>
2024-04-16 20:24:29 +0500

    feat: Implement organization navigation with AccordionTrigger and dynamic routes. and set config for image from clerk in nextj.config

--- src/app/(platform)/(dashboard)/_components/nav-item.tsx
+++ src/app/(platform)/(dashboard)/_components/nav-item.tsx
@@ -1,7 +1,10 @@
 "use client";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { cn } from "@/lib/utils";
+import { Activity, CreditCard, Layout, Settings } from "lucide-react";
 
 import { Accordion } from "@/components/ui/accordion";
-import { cn } from "@/lib/utils";
 import { AccordionItem, AccordionTrigger } from "@radix-ui/react-accordion";
 
 export type Organization = {
@@ -24,14 +27,52 @@ export const NavItem = ({
   organization,
   onExpand,
 }: NavItemProps) => {
+  const router = useRouter();
+  const routes = [
+    {
+      label: "Boards",
+      icon: <Layout className="h-4 w-4 mr-2" />,
+      href: `/organization/${organization.id}`,
+    },
+    {
+      label: "Activity",
+      icon: <Activity className="h-4 w-4 mr-2" />,
+      href: `/organization/${organization.id}/activity`,
+    },
+    {
+      label: "Settings",
+      icon: <Settings className="h-4 w-4 mr-2" />,
+      href: `/organization/${organization.id}/settings`,
+    },
+    {
+      label: "Billing",
+      icon: <Layout className="h-4 w-4 mr-2" />,
+      href: `/organization/${organization.id}/billing`,
+    },
+  ];
   return (
-    <AccordionItem 
-    value={organization.id} 
-    className="border-none">
-      <AccordionTrigger
-        onCanPlay={() => onExpand(organization.id)}
-        className={cn("flex ")}
-      ></AccordionTrigger>
-    </AccordionItem>
+    <Accordion type="multiple">
+      <AccordionItem value={organization.id} className="border-none">
+        <AccordionTrigger
+          onCanPlay={() => onExpand(organization.id)}
+          className={cn(
+            "flex items-center gap-x-2 p-1.5 text-neutral-700 rounded-md hover:bg-neutral-500/10 transition text-start no-underline hover:no-underline ",
+            isActive && !isExpanded && "bg-sky-500/10 text-sky-700"
+          )}
+        >
+          <div className="flex items-center gap-x-2">
+            <div className="w-7 h-7 relative">
+              <Image
+                fill
+                src={organization.imageUrl}
+                alt="Organization"
+                className="rounded-sm object-cover"
+              />
+            </div>
+            <span className="font-medium text-sm">{organization.name}</span>
+          </div>
+        </AccordionTrigger>
+      </AccordionItem>
+    </Accordion>
   );
 };
