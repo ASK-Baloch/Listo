5242bdbb0f2e2ddf5edf51b248a7062331afe6f8
Ahmed <balochsaeed888@gmail.com>
2024-04-16 20:01:54 +0500

    feat: Implement organization navigation item component using AccordionItem (client-side)

--- src/app/(platform)/(dashboard)/_components/nav-item.tsx
+++ src/app/(platform)/(dashboard)/_components/nav-item.tsx
@@ -1,10 +1,37 @@
-"use client"
+"use client";
 
-export const NavItem = () => {
-    return(
-        <div>
-            NavItem
-        </div>
-    )
-            
+import { Accordion } from "@/components/ui/accordion";
+import { cn } from "@/lib/utils";
+import { AccordionItem, AccordionTrigger } from "@radix-ui/react-accordion";
+
+export type Organization = {
+  id: string;
+  name: string;
+  slug: string;
+  imageUrl: string;
+};
+
+interface NavItemProps {
+  isExpanded: boolean;
+  isActive: boolean;
+  organization: Organization;
+  onExpand: (id: string) => void;
 }
+
+export const NavItem = ({
+  isExpanded,
+  isActive,
+  organization,
+  onExpand,
+}: NavItemProps) => {
+  return (
+    <AccordionItem 
+    value={organization.id} 
+    className="border-none">
+      <AccordionTrigger
+        onCanPlay={() => onExpand(organization.id)}
+        className={cn("flex ")}
+      ></AccordionTrigger>
+    </AccordionItem>
+  );
+};
