088f34fdd4a9a08aafefdf21e0902bb5776664d9
Ahmed <balochsaeed888@gmail.com>
2024-04-16 19:21:31 +0500

    feat: Extract default expanded values for accordion based on initial state

--- src/app/(platform)/(dashboard)/_components/sidebar.tsx
+++ src/app/(platform)/(dashboard)/_components/sidebar.tsx
@@ -18,7 +18,23 @@ export const Sidebar = ({ storageKey = "t-sidebar-state" }: SidebarProps) => {
     storageKey,
     {}
   );
-  const { organization: activeOrganization, isLoaded: isLoadedOrg } = useOrganization();
-  const { userMemberships, isLoaded: isLoadedOrgList } = useOrganizationList();
+  const { organization: activeOrganization, isLoaded: isLoadedOrg } =
+    useOrganization();
+  const { userMemberships, isLoaded: isLoadedOrgList } = useOrganizationList({
+    userMemberships: {
+      infinite: true,
+    },
+  });
+
+  const defaultAccordionValue: string[] = Object.keys(expanded).reduce(
+    (acc: string[], key: string) => {
+      if (expanded[key]) {
+        acc.push(key);
+      }
+      return acc;
+    },
+    []
+  );
+
   return <div>Sidebar</div>;
 };
