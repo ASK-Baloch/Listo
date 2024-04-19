f4ca6a13e81f48c2062a8d34261efb169edf690e
Ahmed <balochsaeed888@gmail.com>
2024-04-19 23:44:12 +0500

    addind necessary dependenices

--- src/app/(platform)/(dashboard)/_components/navbar.tsx
+++ src/app/(platform)/(dashboard)/_components/navbar.tsx
@@ -6,7 +6,7 @@ import { FileX, Plus } from "lucide-react";
 export const Navbar = () => {
   return (
     <nav className="fixed z-50 top-0 px-4 w-full h-14 border-b shadow-sm bg-white flex items-center">
-      {/* TODO: MOBILE SIDEBAR */}
+      {/* TODO: MOBILE SIDEBAR */} 
       <div className="flex items-center gap-x-4 ">
         <div className="hidden md:flex">
           <Logo />
