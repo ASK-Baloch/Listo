c967927d21710efa0d8e11bced5b4422823bffde
Ahmed <balochsaeed888@gmail.com>
2024-04-20 23:48:47 +0500

    addded an useEffect hook to record the state

--- src/app/(platform)/(dashboard)/_components/mobile-sidebar.tsx
+++ src/app/(platform)/(dashboard)/_components/mobile-sidebar.tsx
@@ -1,20 +1,22 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
 
 import { useMobileSidebar } from "../../../../../hooks/use-mobile-sidebar";
 
-
 export const MobileSidebar = () => {
-const pathName= usePathname();
-    const [isMounted,setIsMounted]=useState(false)
-    const onOpen=useMobileSidebar((state)=>state.onOpen)
-    const onClose=useMobileSidebar((state)=>state.onOpen)
-    const isOpen=useMobileSidebar((state)=>state.onOpen)
-    return(
-        <div>
-            SIdebar
-        </div>
-    )
-}
+  const pathName = usePathname();
+  const [isMounted, setIsMounted] = useState(false);
+  
+  const onOpen = useMobileSidebar((state) => state.onOpen);
+  const onClose = useMobileSidebar((state) => state.onOpen);
+  const isOpen = useMobileSidebar((state) => state.onOpen);
+  
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if(!isMounted) return null;
+  return <div>SIdebar</div>;
+};
