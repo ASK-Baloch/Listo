7e0fb18cdd7aac665a8369ef2d27110d8cdad588
Ahmed <balochsaeed888@gmail.com>
2024-04-20 23:45:52 +0500

    fixed the bug

--- src/app/(platform)/(dashboard)/_components/mobile-sidebar.tsx
+++ src/app/(platform)/(dashboard)/_components/mobile-sidebar.tsx
@@ -4,8 +4,8 @@ import { useMobileSidebar } from "../../../../../hooks/use-mobile-sidebar";
 
 export const MobileSidebar = () => {
     const onOpen=useMobileSidebar((state)=>state.onOpen)
-    const onOpen=useMobileSidebar((state)=>state.onOpen)
-    const onOpen=useMobileSidebar((state)=>state.onOpen)
+    const onClose=useMobileSidebar((state)=>state.onOpen)
+    const isOpen=useMobileSidebar((state)=>state.onOpen)
     return(
         <div>
             SIdebar
