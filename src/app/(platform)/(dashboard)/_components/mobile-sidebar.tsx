548f5ac2a01193f9cb98de7cffe791457348a275
Ahmed <balochsaeed888@gmail.com>
2024-04-20 23:44:45 +0500

    using hooke usemobilesidebar

--- src/app/(platform)/(dashboard)/_components/mobile-sidebar.tsx
+++ src/app/(platform)/(dashboard)/_components/mobile-sidebar.tsx
@@ -1,6 +1,11 @@
 "use client";
 
+import { useMobileSidebar } from "../../../../../hooks/use-mobile-sidebar";
+
 export const MobileSidebar = () => {
+    const onOpen=useMobileSidebar((state)=>state.onOpen)
+    const onOpen=useMobileSidebar((state)=>state.onOpen)
+    const onOpen=useMobileSidebar((state)=>state.onOpen)
     return(
         <div>
             SIdebar
