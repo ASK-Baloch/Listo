1e1afb6a6c646a478e43041bcbc5d27cb168b01b
Ahmed <balochsaeed888@gmail.com>
2024-04-20 23:47:02 +0500

    added more imports

--- src/app/(platform)/(dashboard)/_components/mobile-sidebar.tsx
+++ src/app/(platform)/(dashboard)/_components/mobile-sidebar.tsx
@@ -1,8 +1,14 @@
 "use client";
 
+import { useState } from "react";
+import { usePathname } from "next/navigation";
+
 import { useMobileSidebar } from "../../../../../hooks/use-mobile-sidebar";
 
+
 export const MobileSidebar = () => {
+const pathName= usePathname();
+    const [isMounted,setIsMounted]=useState(false)
     const onOpen=useMobileSidebar((state)=>state.onOpen)
     const onClose=useMobileSidebar((state)=>state.onOpen)
     const isOpen=useMobileSidebar((state)=>state.onOpen)
