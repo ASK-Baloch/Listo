efc54578321408a9d089ed673bcb17d08d7468a4
Ahmed <balochsaeed888@gmail.com>
2024-05-04 20:43:46 +0500

    added dependency for shadcn ui for input . removed classname as it has prebuild

--- src/app/(platform)/(dashboard)/organization/[organizationId]/form-input.tsx
+++ src/app/(platform)/(dashboard)/organization/[organizationId]/form-input.tsx
@@ -1,18 +1,24 @@
-interface FormInputProps{
-    errors?: {
-        title?: string[]
-    }
+"use client";
+
+import { Input } from "@/components/ui/input";
+import { useFormStatus } from "react-dom";
+
+interface FormInputProps {
+  errors?: {
+    title?: string[];
+  };
 }
 
-export const FormInput = ({errors}:FormInputProps) => {
+export const FormInput = ({ errors }: FormInputProps) => {
+  const { pending } = useFormStatus();
   return (
     <div>
-      <input
+      <Input
         id="title"
         name="title"
         required
         placeholder="Enter a board title"
-        className="border-black border p-1"
+        disabled={pending}
       />
       {errors?.title ? (
         <div>
