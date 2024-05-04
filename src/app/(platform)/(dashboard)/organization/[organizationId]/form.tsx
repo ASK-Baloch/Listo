22b3fc01524f19add51364f30cdc44ac4e7842ff
Ahmed <balochsaeed888@gmail.com>
2024-05-04 20:33:35 +0500

    handling erros in form.tsx , and showing error

--- src/app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
+++ src/app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
@@ -5,17 +5,26 @@ import { create } from "../../../../../../actions/create-board";
 import { useFormState } from "react-dom";
 
 export const Form = () => {
-const initialState = { message: "", errors: {} };
-const [state, dispatch] = useFormState(create, initialState);
+  const initialState = { message: "", errors: {} };
+  const [state, dispatch] = useFormState(create, initialState);
   return (
     <form action={dispatch}>
-      <input
-        id="title"
-        name="title"
-        required
-        placeholder="Enter a board title"
-        className="border-black border p-1"
-      />
+      <div className="flex flex-col space-y-2">
+        <input
+          id="title"
+          name="title"
+          required
+          placeholder="Enter a board title"
+          className="border-black border p-1"
+        />
+        {state?.errors?.title ? (
+            <div>
+                {state.errors.title.map((error: string) => (
+                    <p key={error} className="text-rose-500">{error}</p>
+                ))}
+            </div>
+        ):null}
+      </div>
       <Button type="submit">Submit</Button>
     </form>
   );
