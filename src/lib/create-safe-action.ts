256d25610ce2f9974bc1009ad08047c2a4de1886
Ahmed <balochsaeed888@gmail.com>
2024-05-07 12:10:09 +0500

    a function created called createSafeAction to handle handler function

---
+++ src/lib/create-safe-action.ts
@@ -0,0 +1,26 @@
+import { z } from "zod";
+
+export type FieldErrors<T> = {
+    [K in keyof T]?: string
+}
+
+export type ActionState<TInput,TOuput>={
+    fieldsErrors:FieldErrors<TInput>,
+    error?:string|null,
+    data?:TOuput,
+}
+
+export const createSafeAction = <TInput,TOuput>(
+    schema:z.Schema<TInput>,
+    handler:(validatedData:TInput)=>Promise<ActionState<TInput,TOuput>>
+) => {
+    return async (data:TInput):Promise<ActionState<TInput,TOuput>> => {
+        const validationResult = schema.safeParse(data)
+        if(!validationResult.success){
+           return{
+            fieldsErrors:validationResult.error.flatten().fieldErrors as FieldErrors<TInput>,
+           }
+        }
+    return handler(validationResult.data) 
+    }
+}
