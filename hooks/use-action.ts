b30bc1053691d13491dda37deb3daaf4e68625b2
Ahmed <balochsaeed888@gmail.com>
2024-05-08 22:18:41 +0500

    Enhance use-action hook with options for success, error, and completion callbacks

--- hooks/use-action.ts
+++ hooks/use-action.ts
@@ -3,3 +3,13 @@ import { useState, useCallback } from "react";
 import { ActionState,FieldErrors } from "@/lib/create-safe-action";
 
 type Action<TInput,TOutput> = (data: TInput) => Promise<ActionState<TInput,TOutput>>;
+
+interface UseActionOptions<TOutput>{
+    onSuccess?: (data: TOutput) => void;
+    onError?: (error: string) => void;
+    onComplete?: () => void;
+}
+
+export const useAction = <TInput,TOutput>(action: Action<TInput,TOutput>, options: UseActionOptions<TOutput> = {})=>{
+    const [FieldErrors , setFieldErrors] = useState<FieldErrors<TInput> | undefined>(undefined);
+}
