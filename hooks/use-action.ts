29b37eec8258951677c42336e9fc2f697507134f
Ahmed <balochsaeed888@gmail.com>
2024-05-08 22:21:43 +0500

    Introduce state management for error, data, and loading in use-action hook

--- hooks/use-action.ts
+++ hooks/use-action.ts
@@ -1,15 +1,18 @@
 import { useState, useCallback } from "react";
 
-import { ActionState,FieldErrors } from "@/lib/create-safe-action";
+import { ActionState, FieldErrors } from "@/lib/create-safe-action";
 
-type Action<TInput,TOutput> = (data: TInput) => Promise<ActionState<TInput,TOutput>>;
+type Action<TInput, TOutput> = (data: TInput) => Promise<ActionState<TInput, TOutput>>;
 
-interface UseActionOptions<TOutput>{
+interface UseActionOptions<TOutput> {
     onSuccess?: (data: TOutput) => void;
     onError?: (error: string) => void;
     onComplete?: () => void;
 }
 
-export const useAction = <TInput,TOutput>(action: Action<TInput,TOutput>, options: UseActionOptions<TOutput> = {})=>{
-    const [FieldErrors , setFieldErrors] = useState<FieldErrors<TInput> | undefined>(undefined);
+export const useAction = <TInput, TOutput>(action: Action<TInput, TOutput>, options: UseActionOptions<TOutput> = {}) => {
+    const [FieldErrors, setFieldErrors] = useState<FieldErrors<TInput> | undefined>(undefined);
+    const [error, setError] = useState<string | undefined>(undefined);
+    const [data, setdata] = useState<TOutput | undefined>(undefined);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
 }
