557c0a8375cbeb14da69013717ced8b19e938a57
Ahmed <balochsaeed888@gmail.com>
2024-05-10 23:08:28 +0500

    updated imports in form.tsx

--- src/app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
+++ src/app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
@@ -1,14 +1,10 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { create } from "../../../../../../actions/create-board";
-import { useFormState } from "react-dom";
+import { createBoard } from "../../../../../../actions/create-board";
 import { FormInput } from "./form-input";
 import { FormButton } from "./form-button";
 
 export const Form = () => {
-  const initialState = { message: "", errors: {} };
-  const [state, dispatch] = useFormState(create, initialState);
   return (
     <form action={dispatch}>
       <div className="flex flex-col space-y-2">
