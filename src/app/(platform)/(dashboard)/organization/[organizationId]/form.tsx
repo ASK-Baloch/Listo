23e704304ec8ab82b06ec83ed8ea2b270f0acde9
Ahmed <balochsaeed888@gmail.com>
2024-05-10 23:15:32 +0500

    updated code with abstract actions also error handling too

--- src/app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
+++ src/app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
@@ -3,12 +3,25 @@
 import { createBoard } from "../../../../../../actions/create-board";
 import { FormInput } from "./form-input";
 import { FormButton } from "./form-button";
+import { useAction } from "../../../../../../hooks/use-action";
 
 export const Form = () => {
+  const {execute,fieldErrors} = useAction(createBoard,{
+    onSuccess:(data)=>{
+      console.log(data,"success")
+    },
+    onError:(error)=>{
+      console.log(error)
+    },
+  });
+  const onSubmit = (formData:FormData)=>{
+    const title = formData.get('title') as string;
+    execute({title})
+  }
   return (
-    <form action={dispatch}>
+    <form action={onSubmit }>
       <div className="flex flex-col space-y-2">
-       <FormInput errors={state?.errors} />
+       <FormInput errors={fieldErrors} />
       </div>
      <FormButton />
     </form>
