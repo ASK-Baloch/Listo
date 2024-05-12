80295e3e4b5d648cd79ec9bcda2390d194e2f89d
Ahmed <balochsaeed888@gmail.com>
2024-05-12 23:29:12 +0500

    form-erros.tsx is completed

--- src/components/form/form-errors.tsx
+++ src/components/form/form-errors.tsx
@@ -1,6 +1,4 @@
-
-import { XCircleIcon } from "lucide-react";
-import { cn } from "@/lib/utils";
+import { XCircle } from "lucide-react";
 
 interface FormErrorsProps {
   id: string;
@@ -8,10 +6,22 @@ interface FormErrorsProps {
 }
 
 export const FormErrors = ({ id, errors }: FormErrorsProps) => {
-    if(!errors) return null
-    return(
-        <div id={`${id}-error`} aria-live="polite" className="mt-2 text-sm text-rose-600">
-            
+  if (!errors) return null;
+  return (
+    <div
+      id={`${id}-error`}
+      aria-live="polite"
+      className="mt-2 text-sm text-rose-600"
+    >
+      {errors[id]?.map((error: string) => (
+        <div
+          key={error}
+          className="flex items-center font-medium p-2 border border-rose-500 bg-rose-500/10 rounded-sm"
+        >
+          <XCircle className="w-4 h-4 mr-2" />
+          {error}
         </div>
-    )
-}
+      ))}
+    </div>
+  );
+};
