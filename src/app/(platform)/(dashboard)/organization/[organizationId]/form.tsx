79e427617a5d0fcb9d633d49dc7af9b6d9bc9a0c
Ahmed <balochsaeed888@gmail.com>
2024-05-13 22:58:06 +0500

    changed form-button  with formsubmit that is additionaly more good

--- src/app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
+++ src/app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
@@ -3,7 +3,7 @@
 import { useAction } from "../../../../../../hooks/use-action";
 import { createBoard } from "../../../../../../actions/create-board";
 import { FormInput } from "@/components/form/form-input";
-import { FormButton } from "./form-button";
+import { FormSubmit } from "@/components/form/form-submit";
 
 export const Form = () => {
   const { execute, fieldErrors } = useAction(createBoard, {
@@ -24,7 +24,7 @@ export const Form = () => {
       <div className="flex flex-col space-y-2">
         <FormInput label="Board Title" id='title' errors={fieldErrors}/>
       </div>
-      <FormButton />
+      <FormSubmit>Save</FormSubmit>
     </form>
   );
 };
