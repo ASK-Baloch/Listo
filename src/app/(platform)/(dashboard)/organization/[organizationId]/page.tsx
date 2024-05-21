2700d5f336b84b0c3388cbf967fafb86db67baa0
Ahmed <balochsaeed888@gmail.com>
2024-05-21 23:18:49 +0500

    removing useless code from organization id

--- src/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ src/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,19 +1,7 @@
-import { Button } from "@/components/ui/button";
-import { create } from "../../../../../../actions/create-board";
-import { db } from "@/lib/db";
-import { Board } from "./board";
-import { Form } from "./form";
-
 const OrganizationPage = async () => {
-  const boards = await db.board.findMany();
   return (
     <div className="flex flex-col space-y-4">
-      <Form />
-      <div className="space-y-2">
-        {boards.map((board) => (
-          <Board title={board.title} key={board.id} id={board.id} />
-        ))}
-      </div>
+
     </div>
   );
 };
