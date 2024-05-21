e92163642f33a216b2eeac854910e31af855fb84
Ahmed <balochsaeed888@gmail.com>
2024-05-21 23:36:47 +0500

    added image also removed bug assocatied with src

--- src/app/(platform)/(dashboard)/organization/[organizationId]/_components/info.tsx
+++ src/app/(platform)/(dashboard)/organization/[organizationId]/_components/info.tsx
@@ -1,9 +1,23 @@
-"use client"
+"use client";
+
+import { useOrganization } from "@clerk/nextjs";
+import Image from "next/image";
 
 export const Info = () => {
-    return (
-        <div>
-            Info
-        </div>
-    )
-}
+  const { organization, isLoaded } = useOrganization();
+  if (!isLoaded) {
+    return <p>Loading... </p>;
+  }
+  return (
+    <div className="flex items-center gap-x-4">
+      <div className="w-[60px] h-[60px] relative">
+        <Image
+          fill
+          src={organization?.imageUrl!}
+          alt="organization"
+          className="rounded-md object-cover"
+        />
+      </div>
+    </div>
+  );
+};
